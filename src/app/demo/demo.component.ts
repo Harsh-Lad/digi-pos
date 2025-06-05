848294a4cecaa138f80b1b011182bf86655724c7
Harsh Lad <harshplad@gmail.com>
2025-06-05 17:15:10 +0530

    feat: Add demo component with routing, template, and styles

---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  imports: [],
+  templateUrl: './demo.component.html',
+  styleUrl: './demo.component.css',
+  standalone: true,
+})
+export class DemoComponent {
+
+}

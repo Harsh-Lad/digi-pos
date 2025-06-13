82cbb5dd8d5720a60a330c72f74e389b7bf16c44
harsh <harshplad@gmail.com>
2025-06-13 16:54:06 +0530

    refactor(demo): remove unused ContactFormComponent from imports

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { DemoFormComponent } from '../demo-form/demo-form.component';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { FooterComponent } from "../footer/footer.component";
-import { ContactFormComponent } from "../contact-form/contact-form.component";
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css'],
-  imports: [DemoFormComponent, NavbarComponent, FooterComponent, ContactFormComponent],
+  imports: [DemoFormComponent, NavbarComponent, FooterComponent],
   standalone: true
 })
 export class DemoComponent {
